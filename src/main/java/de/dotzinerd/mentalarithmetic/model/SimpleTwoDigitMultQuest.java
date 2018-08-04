093c90d9775599acf211c8070980e50e3f577eb3
BadaBoomi <heiko.dotzauer@dtzr.de>
2018-08-04 18:36:34 +0200

    Korrektur 2

--- src/main/java/de/dotzinerd/mentalarithmetic/model/SimpleTwoDigitMultQuest.java
+++ src/main/java/de/dotzinerd/mentalarithmetic/model/SimpleTwoDigitMultQuest.java
@@ -1,16 +1,20 @@
 package de.dotzinerd.mentalarithmetic.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class SimpleTwoDigitMultQuest extends Quest {
+	static final Logger logger = LogManager.getLogger(SimpleTwoDigitMultQuest.class);
 
 	public SimpleTwoDigitMultQuest() {
 		int dig1 = ((int) (Math.random() * 9) + 1);
-		int dig2= ((int) (Math.random() * 9) + 1);
-		Integer op1=dig1*10+dig2;
-		Integer op2=dig1*10+(10-dig2);
-		this.answer = op1 * op2;
-		this.question = "Was macht " + op1 + " mal " + op2+"?";
-	}
+		int dig2 = ((int) (Math.random() * 9) + 1);
+		Integer op1 = dig1 * 10 + dig2;
+		Integer op2 = dig1 * 10 + (10 - dig2);
 
-	
+		this.answer = op1 * op2;
+		this.question = "Was macht " + op1 + " mal " + op2 + "?";
+		logger.debug("question:" + this.question + ", answer:" + answer);
+	}
 
 }
