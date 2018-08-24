db1e0d4b9a1f1965e9556b9fe3408aad40c494f9
BadaBoomi <heiko.dotzauer@dtzr.de>
2018-08-24 22:20:02 +0200

    deb

--- src/main/java/de/dotzinerd/mentalarithmetic/enums/Level.java
+++ src/main/java/de/dotzinerd/mentalarithmetic/enums/Level.java
@@ -1,5 +1,8 @@
 package de.dotzinerd.mentalarithmetic.enums;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.dotzinerd.mentalarithmetic.model.quests.AdvancedMultby11Quest;
 import de.dotzinerd.mentalarithmetic.model.quests.Quest;
 import de.dotzinerd.mentalarithmetic.model.quests.SimpleMultby11Quest;
@@ -9,7 +12,11 @@ import de.dotzinerd.mentalarithmetic.model.quests.SimpleTwoDigitSquareQuest;
 public enum Level {
 	LVL_MULT_BY_11_SIMPLE, LVL_MULT_BY_11_ADVANCED, LVL_SIMPLE_MULTIPLICATION, LVL_2DIGIT_SQUARE, LVL_MULT_2DIGIT_SUM_IS_10;
 
+	static final Logger logger = LogManager.getLogger(Level.class);
+	
 	public static Level getLevelByName(String name) {
+		logger.debug("getLevelByName for:"+name);
+		
 		for (Level en : Level.values()) {
 			if (en.name().equals(name)) {
 				return en;
@@ -19,6 +26,7 @@ public enum Level {
 	}
 
 	public static Quest getQuest(Level level) {
+		logger.debug("getQuest for:"+level);
 		switch (level) {
 		case LVL_MULT_BY_11_SIMPLE:
 			return new SimpleMultby11Quest();
