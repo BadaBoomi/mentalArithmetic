f543417affe89a300f2438c39f78e69f93c5deca
BadaBoomi <heiko.dotzauer@dtzr.de>
2018-08-27 22:34:28 +0200

    ar

--- src/main/java/de/dotzinerd/mentalarithmetic/model/Audios.java
+++ src/main/java/de/dotzinerd/mentalarithmetic/model/Audios.java
@@ -3,8 +3,8 @@ package de.dotzinerd.mentalarithmetic.model;
 public class Audios {
 	public static final String AUDIO_WAITINGLOOP = "<audio src='https://s3.amazonaws.com/ask-soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_waiting_loop_30s_01.mp3'/>";
 	public static final String AUDIO_INTRO = "<audio src='https://s3.eu-central-1.amazonaws.com/heioodo/mentalTrainer/intro.mp3'/>";
-	public static final String AUDIO_LESSON_MULT_2DIGIT_SUM_IS_10 = "https://s3.eu-central-1.amazonaws.com/heioodo/mentalTrainer/MULT_BY_11_SIMPLE.mp3";
-	public static final String AUDIO_LESSON_MULT_BY_11_SIMPLE = "https://s3.eu-central-1.amazonaws.com/heioodo/mentalTrainer/MULT_BY_11_SIMPLE.mp3";
-	public static final String AUDIO_LESSON_LVL_MULT_BY_11_ADVANCED = "https://s3.eu-central-1.amazonaws.com/heioodo/mentalTrainer/MULT_BY_11_SIMPLE.mp3";
+	public static final String AUDIO_LESSON_MULT_2DIGIT_SUM_IS_10 = "<audio src='https://s3.eu-central-1.amazonaws.com/heioodo/mentalTrainer/MULT_BY_11_SIMPLE.mp3'/>";
+	public static final String AUDIO_LESSON_MULT_BY_11_SIMPLE = "<audio src='https://s3.eu-central-1.amazonaws.com/heioodo/mentalTrainer/MULT_BY_11_SIMPLE.mp3'/>";
+	public static final String AUDIO_LESSON_LVL_MULT_BY_11_ADVANCED = "<audio src='https://s3.eu-central-1.amazonaws.com/heioodo/mentalTrainer/MULT_BY_11_SIMPLE.mp3'/>";
 
 }
